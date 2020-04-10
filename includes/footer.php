ed46cde57bd5929fe15c57c4becbf7b60c09a103
harivarshan <harivarshan@live.com>
2020-04-10 13:07:51 +0530

    s is the updated footer file

--- includes/footer.php
+++ includes/footer.php
@@ -4,5 +4,7 @@
         <b>All rights reserved</b>
       </div>
       <strong>Copyright &copy; 2018 Brought to You By <a href="https://code-projects.org/">Code-Projects</a></strong>
+        <br>
+        If you really love our ticket service give us a rating <a href="../BlackHats/rating.php">Here</a>
     </div>
 </footer>
